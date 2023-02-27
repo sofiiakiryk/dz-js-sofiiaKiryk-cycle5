831bedba51663d7a8aa49d1e30bb5b68e8c78cbb
sofiiakiryk <107884647+sofiiakiryk@users.noreply.github.com>
2023-02-27 12:02:01 +0300

    Add files via upload

---
+++ select.js
@@ -0,0 +1,11 @@
+for (let i = 3; i <= 30; i++) alert(i);
+let num = 5;
+while (num >= -10) {
+  console.log(num);
+  num--;
+}
+let correct = "1234";
+let password;
+while (correct != password) {
+  password = prompt("enter password");
+}
